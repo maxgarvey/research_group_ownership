c5d7d2a128d89cd820f03de5dd90cbbe9560e38c
Max Garvey <maxgarvey@betamx.(none)>
2012-07-18 15:56:55 -0700

    doesn't run if timestamp newer than modified resources

--- to_file.py
+++ to_file.py
@@ -1,43 +1,79 @@
+'''/to_file.py'''
 from ownership import create_map
+import os
 
 def make_file(filename):
-    #with open(filename, 'w') as fd: #doesn't work with py 2.4
-    fd = open(filename, 'w') #works with py 2.4
+    '''this method takes the name of the output file, determines if
+    it the script should be reevaluated or not. and then does so,
+    outputting to file.'''
+    if os.path.isfile(filename):
+        our_timestamp = os.stat(filename).st_mtime
+        www_timestamp = os.stat('/vol/www').st_mtime
+        share_timestamp = os.stat('/vol/share').st_mtime
+        if (our_timestamp > www_timestamp) and (our_timestamp > share_timestamp):
+            pass
+        else:
+            os.remove(filename)
+            make_file(filename)
+    else:
+        #with open(filename, 'w') as fd: #doesn't work with py 2.4
+        fd = open(filename, 'w') #works with py 2.4
 
-    #share_map = create_map('/vol/share') #don't have sufficient privs for this
-    share_map = {}
-    www_map = create_map('/vol/www') #do have sufficient privs for this
+        #share_map = create_map('/vol/share') #don't have sufficient privs for this
+        share_map = {}
+        www_map = create_map('/vol/www') #do have sufficient privs for this
 
-    #print 'www_map: ' + str(www_map) #debug
+        #print 'www_map: ' + str(www_map) #debug
 
-    www_keys = www_map.keys()
-    share_keys = share_map.keys()
+        www_keys = www_map.keys()
+        share_keys = share_map.keys()
 
-    master_map = {}
+        master_map = {}
 
-    for key in www_keys:
-        print 'key: ' + str(www_map[key].keys()) #debug
-        owners = {}
-        files = www_map[key].keys()
+        for key in www_keys:
+            #print 'key: ' + str(www_map[key].keys()) #debug
+            owners = {}
+            files = www_map[key].keys()
 
-        for f in files:
-            if not www_map[key][f] in owners.keys():
-                owners[www_map[key][f]] = 0
-            else:
-                owners[www_map[key][f]] += 1
+            for f in files:
+                if not www_map[key][f] in owners.keys():
+                    owners[www_map[key][f]] = 0
+                else:
+                    owners[www_map[key][f]] += 1
 
-        winner = 0
-        winner_owner = ''
+            winner = 0
+            winner_owner = ''
 
-        for owner in owners.keys():
-            if owners[owner] >= winner:
-                winner = owners[owner]
-                winner_owner = owner
+            for owner in owners.keys():
+                if owners[owner] >= winner:
+                    winner = owners[owner]
+                    winner_owner = owner
 
-        master_map[key] = winner_owner
-            
-    for f in master_map.keys():
-        #fd.write('{0} , {1}\n'.format(f, master_map[f])) #doesn't work with py 2.4
-        fd.write(f+' , '+master_map[f]+'\n')
+            master_map[key] = winner_owner
 
-    fd.close() #works with py 2.4
+        for key in share_keys:
+            #print 'key: ' + str(www_map[key].keys()) #debug
+            owners = {}
+            files = share_map[key].keys()
+
+            for f in files:
+                if not share_map[key][f] in owners.keys():
+                    owners[www_map[key][f]] = 0
+                else:
+                    owners[www_map[key][f]] += 1
+
+            winner = 0
+            winner_owner = ''
+
+            for owner in owners.keys():
+                if owners[owner] >= winner:
+                    winner = owners[owner]
+                    winner_owner = owner
+
+            master_map[key] = winner_owner
+
+        for f in master_map.keys():
+            #fd.write('{0} , {1}\n'.format(f, master_map[f])) #doesn't work with py 2.4
+            fd.write(f+' , '+master_map[f]+'\n')
+
+        fd.close() #works with py 2.4
