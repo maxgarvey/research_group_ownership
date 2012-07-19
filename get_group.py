09c3de9a6d1172e5637f07106d89bcf348f0107d
Max Garvey <maxgarvey@betamx.(none)>
2012-07-19 09:41:03 -0700

    streamlined. you just have to run the get method of get_group.py

--- get_group.py
+++ get_group.py
@@ -1,12 +1,17 @@
 '''/get_group.py'''
 from csv import reader
+from to_file import make_file
 
-def get(filename, csv_file):
+def get(filename, csv_file='csv_file.csv'):
     '''this method takes the filename you want the ownership of and
     the csv file's name that you're looking at. It returns the group
     that owns the file that you are interested in.'''
+    make_file(csv_file)
+
     csv_reader = reader(open(csv_file))
     csv_dic = {}
+
     for line in csv_reader:
         csv_dic[line[0].strip()] = line[1].strip()
+
     return csv_dic[filename]
