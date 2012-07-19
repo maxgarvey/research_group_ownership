d68c01c674952a203b1c14bae876b0b50f4c73a5
Max Garvey <maxgarvey@betamx.(none)>
2012-07-19 16:13:58 -0700

    changed some stuff to better work with the autofs stuff.

--- ownership.py
+++ ownership.py
@@ -1,33 +1,86 @@
 import os
 import grp
-#from tempfile import TemporaryFile #doesn't work in py 2.4
+import pwd
+import subprocess
 
 def create_map(location='/vol/www/'):
     '''create_map looks into the /vol/www directory and examines each item therein
     entering each directory and getting the permissions of the files inside.'''
     dir_dict = {}
 
+    #get all the permissions to bubble up just in case
+    location_asterisk = os.path.join(location,'*')
+    subprocess.call(['ls -l '+location_asterisk], shell=True)
+
+    all_users_v = pwd.getpwall()
+    all_users = []
+    for user in all_users_v:
+        all_users.append(user[0])
+
+    #get a list of the contents of location... which will just be directories
     dirs = os.listdir(location)
+
+    #print 'len(dirs): '+str(len(dirs)) #debug
     for _dir in dirs:
         dir_dict[_dir] = {}
 
+    #for each of the directories inside of location,
     for _dir in dirs:
-        #with TemporaryFile() as temp: doesn't work in py 2.4
-
+        #first try to get the permissions of the directory itself
         try:
-            dir_contents = os.listdir(os.path.join(location,_dir))
+            gid_number = os.stat(os.path.join(location, _dir)).st_gid
+            groupname = grp.getgrgid(gid_number)[0]
+            #print 'dir: ' + _dir  + ', groupname: ' + groupname #debug
         except Exception, err:
-            print 'couldn\'t open ' + str(os.path.join(location, _dir)) + '\n' + str(err)
+            groupname = ''
+            print 'couldn\'t stat ' + str(os.path.join(location, _dir))
 
-        for subdir in dir_contents:
+        found_groupname = False
+
+        #if it is a proper groupname (not a user's group), keep it
+        try:
+            user = pwd.getpwnam(groupname)
+        except:
+            found_groupname = True
+            dir_dict[_dir] = groupname
+
+        #otherwise, look at the contents to figure out who the group is
+        if not found_groupname:
             try:
-                gid_number = os.stat(os.path.join(os.path.join(location,_dir),subdir)).st_gid
-                dir_dict[_dir][subdir] = grp.getgrgid(gid_number)[0]
+                #get the contents of that directory
+                dir_contents = os.listdir(os.path.join(location,_dir))
             except Exception, err:
-                print 'couldn\'t stat ' + str(os.path.join(os.path.join(location, _dir), subdir))
+                dir_contents = []
+                print 'couldn\'t open ' + str(os.path.join(location, _dir)) + '\n' + str(err)
 
+            owners = {}
+            #for each of the items inside the directory
+            for subdir in dir_contents:
+                try:
+                    #stat the file for gid
+                    gid_number = os.stat(os.path.join(os.path.join(location,_dir),subdir)).st_gid
+                    #and enter into our structure
+                    if grp.getgrgid(gid_number)[0] in owners:
+                        owners[grp.getgrgid(gid_number)[0]] += 1
+                    else:
+                        owners[grp.getgrgid(gid_number)[0]] = 1
+                except Exception, err:
+                    print 'couldn\'t stat ' + str(os.path.join(os.path.join(location, _dir), subdir))
+
+            winner_number = 0
+            winner_owner = ''
+            for owner in owners.keys():
+                if owners[owner] > winner_number:
+                    winner_owner = owner
+                    winner_number = owners[owner]
+            dir_dict[_dir] = winner_owner
+
+    #remove empty elements
+    deleted_keys = []
     for key in dir_dict.keys():
-        if dir_dict[key] == {}:
+        if dir_dict[key] == '':
+            deleted_keys.append(key)
             del(dir_dict[key])
+    print 'deleted keys: ' + str(deleted_keys)
 
     return dir_dict
--- to_file.py
+++ to_file.py
@@ -6,24 +6,35 @@ def make_file(filename):
     '''this method takes the name of the output file, determines if
     it the script should be reevaluated or not. and then does so,
     outputting to file.'''
+
+    #check to see if we have to run the lookup again, or if our current
+    #file is more recent than the most recent change
     if os.path.isfile(filename):
-        our_timestamp = os.stat(filename).st_mtime
-        www_timestamp = os.stat('/vol/www').st_mtime
-        share_timestamp = os.stat('/vol/share').st_mtime
+        our_timestamp = os.stat(filename).st_mtime       #time stamp of csv file
+        www_timestamp = os.stat('/vol/www').st_mtime     #time stamp of www
+        share_timestamp = os.stat('/vol/share').st_mtime #time stamp of share
+
+        #is our timestamp newer than the others?
         if (our_timestamp > www_timestamp) and (our_timestamp > share_timestamp):
             pass
+
+        #if not, make a new one!
         else:
             os.remove(filename)
             make_file(filename)
+
+    #if the file doesn't already exist (or has just been erased), then
+    #we will create a new one.
     else:
         #with open(filename, 'w') as fd: #doesn't work with py 2.4
         fd = open(filename, 'w') #works with py 2.4
 
-        #share_map = create_map('/vol/share') #don't have sufficient privs for this
-        share_map = {}
+        share_map = create_map('/vol/share') #don't have sufficient privs for this
+        print '\nlen_share_map: ' + str(len(share_map)) + '\n' #debug
+        #share_map = {}
         www_map = create_map('/vol/www') #do have sufficient privs for this
 
-        #print 'www_map: ' + str(www_map) #debug
+        print '\nlen_www_map: ' + str(len(www_map)) + '\n' #debug
 
         www_keys = www_map.keys()
         share_keys = share_map.keys()
@@ -58,9 +69,9 @@ def make_file(filename):
 
             for f in files:
                 if not share_map[key][f] in owners.keys():
-                    owners[www_map[key][f]] = 0
+                    owners[share_map[key][f]] = 0
                 else:
-                    owners[www_map[key][f]] += 1
+                    owners[share_map[key][f]] += 1
 
             winner = 0
             winner_owner = ''
@@ -74,6 +85,6 @@ def make_file(filename):
 
         for f in master_map.keys():
             #fd.write('{0} , {1}\n'.format(f, master_map[f])) #doesn't work with py 2.4
-            fd.write(f+' , '+master_map[f]+'\n')
+            fd.write(f + ' , ' + master_map[f] + '\n')
 
         fd.close() #works with py 2.4
